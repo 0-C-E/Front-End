337042e9979ac6cc5be35bf1052f183c55374fe6
Vianpyro <vianney@veremme.org>
2024-11-19 14:48:25 -0500

    Enhance login and registration forms with improved layout, styling, and password visibility toggle

--- js/index.js
+++ js/index.js
@@ -1,22 +1,36 @@
 const flipper = document.getElementsByTagName("main")[0];
 const goToRegisterButton = document.getElementById("go-to-register");
 const goToLoginButton = document.getElementById("go-to-login");
+const showPasswordButtons = document.getElementsByClassName("show-password");
+const loginSection = document.getElementById('login');
+const registerSection = document.getElementById('register');
 
 function toggleFlipper(side) {
     if (flipper.style.transform === "rotateY(180deg)" || side === "register") {
         flipper.style.transform = "rotateY(0deg)";
         setTimeout(() => {
-            loginForm.style.display = "flex";
-            registerForm.style.display = "none";
+            loginSection.style.display = "flex";
+            registerSection.style.display = "none";
         }, 250);
     } else {
         flipper.style.transform = "rotateY(180deg)";
         setTimeout(() => {
-            loginForm.style.display = "none";
-            registerForm.style.display = "flex";
+            loginSection.style.display = "none";
+            registerSection.style.display = "flex";
         }, 250);
     }
 }
 
 goToRegisterButton.addEventListener("click", () => toggleFlipper("register"));
 goToLoginButton.addEventListener("click", () => toggleFlipper("login"));
+
+Array.from(showPasswordButtons).forEach(button => {
+    button.addEventListener("click", () => {
+        const passwordInput = document.getElementById("register-password");
+        const passwordConfirmInput = document.getElementById("register-password-confirm");
+
+        const type = passwordInput.type === "password" ? "text" : "password";
+        passwordInput.type = type;
+        passwordConfirmInput.type = type;
+    });
+});
