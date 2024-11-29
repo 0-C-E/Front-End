703d2049663b5c053a0299ab344ad1363ff2d6c6
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-11-29 12:32:47 -0500

    Add email input and password strength feedback to registration form (#1)

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const goToLoginButton = document.getElementById("go-to-login");
 const showPasswordChecks = document.querySelectorAll(".show-password-check");
 const loginSection = document.getElementById('login');
 const registerSection = document.getElementById('register');
+const passwordFeedback = document.getElementById('password-feedback');
+const passwordInput = document.getElementById('register-password');
+const passwordStrengthBar = document.getElementById('password-strength');
 
 function toggleFlipper(side) {
     if (flipper.style.transform === "rotateY(180deg)" || side === "register") {
@@ -38,4 +41,30 @@ showPasswordChecks.forEach(checkbox => {
             otherCheckbox.checked = checkbox.checked;
         });
     });
+});
+
+passwordInput.addEventListener("keyup", () => {
+    /*
+    Rates a password based on the following criteria:
+    - At least 12 characters long.
+    - Contains at least one uppercase letter(A - Z).
+    - Contains at least one lowercase letter(a - z).
+    - Contains at least one digit(0 - 9).
+    - Contains at least one special character(any non - alphanumeric character).
+    */
+    const maxPoints = 5;
+    let password = passwordInput.value;
+    const criteria = {
+        length: password.length >= 12,
+        uppercase: /[A-Z]/.test(password),
+        lowercase: /[a-z]/.test(password),
+        digit: /[0-9]/.test(password),
+        special: /[^A-Za-z0-9]/.test(password)
+    }
+    const metCriteriaCount = Object.values(criteria).filter(Boolean).length;
+
+    // Update the strength bar
+    passwordStrengthBar.max = maxPoints;
+    passwordStrengthBar.value = metCriteriaCount;
+    passwordFeedback.style.display = metCriteriaCount > 0 ? "flex" : "none";
 });
